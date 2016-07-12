8200d7569f50c0af8943d3590da962edf52fcacc
Peeyush Jain <jain.peeyush@quikr.com>
2016-07-12 09:35:09 +0530

    setting changed

--- tracks/settings.py
+++ tracks/settings.py
@@ -112,7 +112,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'tracks/static')
+STATIC_ROOT = os.path.join(BASE_DIR, '/static')
 
 REST_FRAMEWORK = {
     'PAGE_SIZE': 10
