7e6802a7b1e3112b972ea4dfb07b01a8860f368b
Peeyush Jain <jain.peeyush@quikr.com>
2016-07-12 09:44:21 +0530

    setting changed

--- tracks/settings.py
+++ tracks/settings.py
@@ -110,9 +110,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/tracks/static/'
+STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'tracks/static')
+STATIC_ROOT = os.path.join(BASE_DIR, '/static')
 
 
 REST_FRAMEWORK = {
